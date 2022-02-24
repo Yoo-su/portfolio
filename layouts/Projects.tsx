31ca01c1564824fa3561bba9ea543508a0674b6b
유수현 <rhan0871@naver.com>
2022-02-24 19:45:21 +0900

    few changes

--- layouts/Projects.tsx
+++ layouts/Projects.tsx
@@ -11,14 +11,14 @@ export default function Projects() {
     const data=myJson.restaurantProject;
     const data2=myJson.welfare_project;
     return (
-        <div id={styles.projects} ref={ref}>
+        <div id={styles.projects}>
             <div id={styles.title}>
                 <IoCodeWorkingSharp className={styles.titleIcon} />
                 <h1>
                     <span>Projects</span>
                 </h1>
             </div>
-            <div id={inView===true?styles.content:styles.hiddenContent}>
+            <div ref={ref} id={inView===true?styles.content:styles.hiddenContent}>
                 <ProjectCard pageInfo={data.pageInfo} year={data.year} name={data.name} stack={data.stack} />
                 <ProjectCard pageInfo={data2.pageInfo} year={data2.year} name={data2.name} stack={data2.stack} />
             </div>
