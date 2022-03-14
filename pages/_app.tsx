2eee1c042faf70a86fa095d249c903d65e171928
유수현 <rhan0871@naver.com>
2022-03-15 02:10:27 +0900

    few changes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,19 @@
 import type { AppProps } from 'next/app'
-import {GlobalStyle} from './styles';
-import './app.css'
+import {createGlobalStyle} from "styled-components";
+import '../styles/app.css'
+
+const GlobalStyle=createGlobalStyle`
+  body{
+    margin:0;
+  }
+
+  main{
+    display:flex;
+    flex-direction:column;
+    justify-content:center;
+    align-items:center;
+  }
+`;
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
